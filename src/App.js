b3d4baefe38ffb679e108556fbe00030e879c6ff
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2019-04-19 12:17:17 +0200

    Initial Commit. Add Grommet & Styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Box, Button } from 'grommet';
+import styled from 'styled-components'
+
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: 'Roboto',
+      size: '20px',
+      height: '20px',
+    },
+  },
+};
+
+const StyledButton = styled(Button)`
+  border-radius: 20px;
+`
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='xlarge'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grommet theme={theme}>
+        <AppBar>
+          Hello World!
+        </AppBar>
+        <StyledButton
+          label="Edit"
+          onClick={() => {}}
+        />
+      </Grommet>
     );
   }
 }
