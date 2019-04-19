67e1dacad1494a4e23cdb809b8f4a54735a4a7ec
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2019-04-19 18:42:33 +0200

    Add eslint and prettier config. Install packages

--- src/App.js
+++ src/App.js
@@ -1,52 +1,49 @@
-import React, { Component } from 'react';
-import { Grommet, Box, Button } from 'grommet';
-import styled from 'styled-components'
+import React, { Component } from "react"
+import { Grommet, Box, Button } from "grommet"
+import styled from "styled-components"
 
 const theme = {
   global: {
     colors: {
-      brand: '#228BE6',
+      brand: "#228BE6"
     },
     font: {
-      family: 'Roboto',
-      size: '20px',
-      height: '20px',
-    },
-  },
-};
+      family: "Roboto",
+      size: "20px",
+      height: "20px"
+    }
+  }
+}
 
 const StyledButton = styled(Button)`
   border-radius: 20px;
 `
 
-const AppBar = (props) => (
+const AppBar = props => (
   <Box
-    tag='header'
-    direction='row'
-    align='center'
-    justify='between'
-    background='brand'
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation='xlarge'
-    style={{ zIndex: '1' }}
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="xlarge"
+    style={{ zIndex: "1" }}
     {...props}
   />
-);
+)
 
 class App extends Component {
   render() {
+    const { option } = this.state
+
     return (
       <Grommet theme={theme}>
-        <AppBar>
-          Hello World!
-        </AppBar>
-        <StyledButton
-          label="Edit"
-          onClick={() => {}}
-        />
+        <AppBar>Hello World!</AppBar>
+        <StyledButton label="Edit" option={option} onClick={() => {}} />
       </Grommet>
-    );
+    )
   }
 }
 
-export default App;
+export default App
