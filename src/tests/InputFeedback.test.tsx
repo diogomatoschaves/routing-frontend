2455a8b99f20d4498f34b0d203eda111254cac3b
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2019-05-20 19:56:12 +0200

    Add integration tests

---
+++ src/tests/InputFeedback.test.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+import { render, cleanup } from 'react-testing-library'
+import { Input } from 'semantic-ui-react'
+// import 'jest-dom/extend-expect'
+import App from '../components/App'
+
+
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  GeolocateControl: jest.fn(),
+  Map: jest.fn(() => ({
+    addControl: jest.fn(),
+    on: jest.fn(),
+    remove: jest.fn()
+  })),
+  NavigationControl: jest.fn()
+}));
+
+const delay = (ms: number) =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+
+describe('Start point Input updates App state on Blur', () => {
+
+  test('', async () => {
+
+    const testInstance = TestRenderer.create(<App />)
+    const root = testInstance.root
+    
+    const input = root.findAllByType(Input);
+
+    input[0].props.onChange('', { value: 'test' })
+    await delay(500)
+
+    input[0].props.onBlur()
+
+    const AppComponent = root.findByType(App);
+
+    expect(AppComponent.instance.state.startPoint).toBe('test');
+
+  })
+})
+
+describe('End point Input updates App state on Blur', () => {
+
+  test('', async () => {
+
+    const testInstance = TestRenderer.create(<App />)
+    const root = testInstance.root
+    
+    const input = root.findAllByType(Input);
+
+    input[1].props.onChange('', { value: 'test' })
+    await delay(500)
+
+    input[1].props.onBlur()
+
+    const AppComponent = root.findByType(App);
+
+    expect(AppComponent.instance.state.endPoint).toBe('test');
+
+  })
+})
+
