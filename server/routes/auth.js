823c155c1fec5967b708c433b2e44cd0363e204f
diogomatoschaves <diogo_chaves@hotmail.com>
2020-02-07 11:20:51 +0100

    Remove unrequired files

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable func-names */
-const http = require('http')
-
-const auth = async (req, res) => {
-  const basicAuth = req.get('Authorization')
-  return res.status(200).json({ basicAuth })
-}
-
-module.exports = auth
